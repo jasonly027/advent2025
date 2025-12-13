5c741e435cbd7c980cc427f22169af3a81c9d382
Jason Ly <81354745+jasonly027@users.noreply.github.com>
2025-12-12 20:13:58 -0800

    Day 10 P2 TLE

--- src/day10.rs
+++ src/day10.rs
@@ -1,5 +1,4 @@
-use std::collections::HashSet;
-
+use fxhash::FxHashSet;
 use itertools::Itertools;
 
 #[derive(Debug)]
@@ -52,7 +51,7 @@ impl Machine {
     }
 
     fn solve(&self) -> u32 {
-        let mut states = HashSet::from([self.lights]);
+        let mut states = FxHashSet::from_iter([self.lights]);
 
         let mut presses = 0;
         while !states.contains(&self.target) {
@@ -74,8 +73,86 @@ pub fn p1(input: &str) -> u32 {
         .sum()
 }
 
-pub fn p2(_input: &str) -> u32 {
-    todo!()
+#[derive(Debug, Clone)]
+struct Machine2 {
+    current: Vec<u32>,
+    buttons: Vec<Vec<usize>>,
+    target: Vec<u32>,
+}
+
+impl Machine2 {
+    fn from_str(str: &str) -> Self {
+        let mut iter = str.split_whitespace().skip(1).peekable();
+
+        let buttons = iter
+            .peeking_take_while(|l| l.starts_with('('))
+            // Strip brackets
+            .map(|raw| &raw[1..raw.len() - 1])
+            // Separate into num vec
+            .map(|field| {
+                field
+                    .split(',')
+                    .map(|b| {
+                        b.parse::<usize>()
+                            .expect("field in button group should be a number")
+                    })
+                    .collect_vec()
+            })
+            .collect_vec();
+
+        let target_str = {
+            let field = iter.next().expect("should have joltage target field");
+            &field[1..field.len() - 1] // Strip brackets
+        };
+
+        let target = target_str
+            .split(',')
+            .map(|field| {
+                field
+                    .parse()
+                    .expect("field in joltage target should be num")
+            })
+            .collect_vec();
+
+        Self {
+            current: vec![0; target.len()],
+            buttons,
+            target,
+        }
+    }
+
+    fn solve(&self) -> u32 {
+        let mut states = FxHashSet::from_iter([self.current.clone()]);
+
+        let mut presses = 0;
+        while !states.contains(&self.target) {
+            let curr_states = states.drain().collect_vec();
+            for (state, btn) in curr_states.iter().cartesian_product(self.buttons.iter()) {
+                let mut new_state = state.clone();
+                for &b in btn {
+                    new_state[b] += 1;
+                }
+
+                if new_state
+                    .iter()
+                    .zip(&self.target)
+                    .all(|(curr, target)| curr <= target)
+                {
+                    states.insert(new_state);
+                }
+            }
+            presses += 1;
+        }
+        dbg!(presses)
+    }
+}
+
+pub fn p2(input: &str) -> u32 {
+    input
+        .lines()
+        .map(Machine2::from_str)
+        .map(|m| m.solve())
+        .sum()
 }
 
 #[cfg(test)]
@@ -98,9 +175,10 @@ mod tests {
 
     #[test]
     fn sample_p2() {
-        assert_eq!(7, p2(include_str!("day10.sample.txt")));
+        assert_eq!(33, p2(include_str!("day10.sample.txt")));
     }
 
+    // TLE
     #[test]
     fn test_p2() {
         let ans = p2(include_str!("day10.txt"));
